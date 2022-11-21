a027dc308fdb81962b7b1e4d7377838cb0a23018
Utkarsh Dwivedi <89937145+utkarsh-00007@users.noreply.github.com>
2022-11-21 19:11:07 +0530

    Commit 16 - Made function of deleting post

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -86,4 +86,12 @@ class FirestoreMethods {
       print(e.toString());
     }
   }
+
+  Future<void> deletePost(String postId) async {
+    try {
+      await _firestore.collection('posts').doc(postId).delete();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
